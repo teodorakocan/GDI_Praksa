b1959f2677ed8d119560ae3c8a72ea2cbb6fcb99
teodorakocan <53410490+teodorakocan@users.noreply.github.com>
2020-11-30 20:09:43 +0100

    added new function

--- praksa.js
+++ praksa.js
@@ -5,7 +5,6 @@ const COUNTIES_ID = 2;
 const STATES_ID = 3;
 const LAYERS = 4;
 
-let layers = [];
 let expression;
 
 require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/symbols/SimpleMarkerSymbol"], function(Map, MapView, FeatureLayer, SimpleMarkerSymbol) {
@@ -34,7 +33,7 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
         container: "viewDiv",
         map: map,
         center: [-96.571671, 39.183609], //-95, 39, 37, 36
-        zoom: 13 //4
+        zoom: 13 //13
     });
 
     for (let i = 0; i < LAYERS; i++) {
@@ -42,41 +41,44 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
             url: URL + i
         });
         map.add(layer);
-        layers.push(layer);
     }
 
-    layers[POINT_ID].outFields = ["HOUSEHOLDS", "STATE_FIPS"];
-    layers[POINT_ID].popupTemplate = {
+    function getLayer(layerIndex) {
+        return map.layers.items[layerIndex];
+    }
+
+    getLayer(POINT_ID).outFields = ["HOUSEHOLDS", "STATE_FIPS"];
+    getLayer(POINT_ID).popupTemplate = {
         title: "{HOUSEHOLDS}",
         content: "{STATE_FIPS}"
     }
-    layers[GROUP_ID].outFields = ["FEMALES", "AGE_22_29", "STATE_FIPS"];
-    layers[GROUP_ID].popupTemplate = {
+    getLayer(GROUP_ID).outFields = ["FEMALES", "AGE_22_29", "STATE_FIPS"];
+    getLayer(GROUP_ID).popupTemplate = {
         title: "{STATE_FIPS}",
         content: "Number of females population is {FEMALES}, number of population between 29 to 29 is {AGE_22_29}"
     }
-    layers[COUNTIES_ID].outFields = ["NAME", "STATE_NAME", "CNTY_FIPS"];
-    layers[COUNTIES_ID].popupTemplate = {
+    getLayer(COUNTIES_ID).outFields = ["NAME", "STATE_NAME", "CNTY_FIPS"];
+    getLayer(COUNTIES_ID).popupTemplate = {
         title: "{NAME}",
         content: "{STATE_NAME}, {CNTY_FIPS}"
     }
-    layers[STATES_ID].outFields = ["STATE_NAME", "STATE_FIPS", "SUB_REGION", "STATE_ABBR"];
-    layers[STATES_ID].popupTemplate = {
+    getLayer(STATES_ID).outFields = ["STATE_NAME", "STATE_FIPS", "SUB_REGION", "STATE_ABBR"];
+    getLayer(STATES_ID).popupTemplate = {
         title: "{STATE_NAME}, {STATE_FIPS}",
         content: "{SUB_REGION}, {STATE_ABBR}"
     }
 
     blockPointLayerToggle.addEventListener("change", function() {
-        layers[POINT_ID].visible = blockPointLayerToggle.checked;
+        getLayer(POINT_ID).visible = blockPointLayerToggle.checked;
     });
     blockGroupToggle.addEventListener("change", function() {
-        layers[GROUP_ID].visible = blockGroupToggle.checked;
+        getLayer(GROUP_ID).visible = blockGroupToggle.checked;
     });
     countiesToggle.addEventListener("change", function() {
-        layers[COUNTIES_ID].visible = countiesToggle.checked;
+        getLayer(COUNTIES_ID).visible = countiesToggle.checked;
     });
     stateToggle.addEventListener("change", function() {
-        layers[STATES_ID].visible = stateToggle.checked;
+        getLayer(STATES_ID).visible = stateToggle.checked;
     });
     basemapToggle.addEventListener("change", function() {
         if (!basemapToggle.checked) {
@@ -113,7 +115,7 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
             }
         }
 
-        layers[POINT_ID].definitionExpression = expression;
+        getLayer(POINT_ID).definitionExpression = expression;
     });
 
     filterGroup.addEventListener("click", function() {
@@ -127,7 +129,7 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
             expression = filter + " " + operator + " " + groupValue;
         }
 
-        layers[GROUP_ID].definitionExpression = expression;
+        getLayer(GROUP_ID).definitionExpression = expression;
     });
 
     selectedCountieFilter.addEventListener("change", function() {
@@ -157,7 +159,7 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
             expression = filter + " " + operator + " '" + countieValue + "'";
         }
 
-        layers[COUNTIES_ID].definitionExpression = expression;
+        getLayer(COUNTIES_ID).definitionExpression = expression;
     });
 
     filterState.addEventListener("click", function() {
@@ -167,7 +169,7 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
 
         expression = filter + " " + operator + " '" + stateValue + "'";
 
-        layers[STATES_ID].definitionExpression = expression;
+        getLayer(STATES_ID).definitionExpression = expression;
     });
 
     function addOperator(operators) {
@@ -192,15 +194,15 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sym
     }
 
     refreshPoints.addEventListener("click", function() {
-        layers[POINT_ID].definitionExpression = "";
+        getLayer(POINT_ID).definitionExpression = "";
     });
     refreshGroup.addEventListener("click", function() {
-        layers[GROUP_ID].definitionExpression = "";
+        getLayer(GROUP_ID).definitionExpression = "";
     });
     refreshCounties.addEventListener("click", function() {
-        layers[COUNTIES_ID].definitionExpression = "";
+        getLayer(COUNTIES_ID).definitionExpression = "";
     });
     refreshStates.addEventListener("click", function() {
-        layers[STATES_ID].definitionExpression = "";
+        getLayer(STATES_ID).definitionExpression = "";
     });
 });
